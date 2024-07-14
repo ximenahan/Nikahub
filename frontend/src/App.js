42358bf90741d96b16e6b6d42f738163d295625c
Ximena <ximenahan00@gmail.com>
2024-07-14 14:04:07 +0800

    Initial settings for frontend tested and completed to start further development

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
 import Canvas from './components/Canvas/Canvas';
-import logo from './logo.svg';
-import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-200 min-h-screen flex justify-center items-center overflow-hidden">
       <Canvas />
     </div>
   );
